6de8ebefe83767affb865f2b5720d5fa67bea4c4
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-04 13:42:03 +0530

    connecting the controller functions

--- backend/routes/goalroutes.js
+++ backend/routes/goalroutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
+router.route('/').get(getGoals).post(setGoal);
+router.route('/:id').put(updateGoal).delete(deleteGoal);
+
+
 router.get('/', getGoals);
 
 router.post('/', setGoal);

77c25c13e935d3fbe7d1072152a82509c915148c
L-Pircy <taveesha.agarwal@gmail.com>
2024-02-29 12:34:54 +0530

    routes, controller and chainign routes

--- backend/routes/goalroutes.js
+++ backend/routes/goalroutes.js
@@ -1,17 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
 
-router.route('/').get(getGoals).post(setGoal);
-router.route('/:id').put(updateGoal).delete(deleteGoal);
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").put(updateGoal).delete(deleteGoal);
 
+module.exports = router;
 
-router.get('/', getGoals);
+// DEPRECATED
 
-router.post('/', setGoal);
+// router.get("/", (req, res) => {
+//   res.status(200).json({ message: "getGoals" });
+// });
+// router.post("/", (req, res) => {
+//   res.status(200).json({ message: "Set Goals" });
+// });
+// router.put("/:id", (req, res) => {
+//   res.status(200).json({ message: `Update Goal ${req.params.id}` });
+// });
+// router.delete("/:id", (req, res) => {
+//   res.status(200).json({ message: `Delete Goal ${req.params.id}` });
+// });
 
-router.put('/:id', updateGoal);
-
-router.delete('/:id', deleteGoal);
-
-module.exports = router
+// router.get("/", getGoals);
+// router.post("/", setGoal);
+// router.put("/:id", updateGoal);
+// router.delete("/:id", deleteGoal);
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const port = process.env.PORT || 5001
+const express = require("express"); //like import
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5001;
 
-const app = express()
+const app = express();
 
-app.use(express.json())//to get values from body as they are undefined warna
-app.use(express.urlencoded({ extended: false })) //middleware to accept data from body
+// app.use(express.json()); //to get values from body as they are undefined warna
+// app.use(express.urlencoded({ extended: false })); //middleware to accept data from body
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+app.use("/api/goals", require("./routes/goalRoutes"));
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`));

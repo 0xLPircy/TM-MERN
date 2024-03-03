fcb2fdca2d6b4f56750c02bfd7f0f4c135f79f6f
L-Pircy <taveesha.agarwal@gmail.com>
2024-03-03 14:52:25 +0530

    router and header/nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Dashboard, Login, Register } from "./pages";
+import { Header } from "./components";
+
 function App() {
   return (
-    <div>
-      <h1>HELLO</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 

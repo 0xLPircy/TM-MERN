1efaa8594baaa60304b042c5eaf5cde7ea8b1773
L-Pircy <taveesha.agarwal@gmail.com>
2024-03-06 11:15:17 +0530

    auth slice ka registration and extra reducers to handle response

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,21 @@
+// service isfor making https request and sending data back and forth
+
+import axios from "axios";
+
+const API_URL = "/api/users";
+
+const register = async (userData) => {
+  const response = await axios.post(API_URL, userData);
+  //   POST REQUEST SE RETURN
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+const authService = {
+  register,
+};
+
+export default authService;

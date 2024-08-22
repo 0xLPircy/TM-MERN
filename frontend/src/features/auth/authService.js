53cb3dee8a64d5a350afbaa31b0dfd972df58d03
0xLPircy <taveesha.agarwal@gmail.com>
2024-08-22 11:13:56 +0530

    login

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const API_URL = "/api/users";
+const API_URL = "/api/users/";
 
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
@@ -14,8 +14,21 @@ const register = async (userData) => {
   return response.data;
 };
 
+const login = async (userData) => {
+  const response = await axios.post(API_URL + 'login', userData);
+  if(response.data){
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+}
+
+const logout =  () => {
+  localStorage.removeItem("user")
+} 
+
 const authService = {
   register,
+  logout,
+  login
 };
 
 export default authService;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import {useSelector, useDispatch} from "react-redux"
+import {useNavigate} from "react-router-dom"
+import {toast} from "react-toastify"
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner.jsx"
 import { FaSignInAlt } from "react-icons/fa";
+
 const Login = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
   const { email, password } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const {user, isError, isLoading, isSuccess, message} = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if(isError){
+      toast.error(message)
+    }
+    if(isSuccess || user){
+      navigate("/")
+    }
+     dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+  
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -15,7 +37,17 @@ const Login = () => {
   };
   const onSubmit = (e) => {
     e.preventDefault();
+    const userData = {
+      email, password
+    }
+
+    dispatch(login(userData))
   };
+
+  if(isLoading){
+    return <Spinner/>
+  }
+
   return (
     <>
       <section className="heading">
